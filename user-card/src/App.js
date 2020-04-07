1a59905125cd5c8ed716e4acb197dbcb9ea4ab2a
Maria Trujillo <mptruj015@gmail.com>
2020-04-08 01:22:08 +0200

    reached mvp

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,37 +1,59 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import UserCard from "./UserCard"
 
 
-class App extends React.Component{
-  constructor(){
+class App extends React.Component {
+  constructor() {
     super();
     this.state = {
-      user: []
+      user: [],
+      followers: []
     };
   }
 
-//step1 fetch user data DONE
-componentDidMount(){
-  axios.get("https://api.github.com/users/mtruj013")
-  .then(response => {
-    console.log(response.data)
-    this.setState({
-      user: response.data.message//set response to state
-    })
-  })
-  .catch(error =>{
-    console.log("couldn't get the data", error);
-  })
-}
+  //step1 fetch user data DONE
+  componentDidMount() {
+    axios.get("https://api.github.com/users/mtruj013")
+      .then(response => {
+        console.log(response.data)
+        this.setState({
+          user: response.data//set response to state
+        })
+      })
+      .catch(error => {
+        console.log("couldn't get the data", error);
+      })
+    axios.get("https://api.github.com/users/mtruj013/followers")
+      .then(response => {
+        console.log(response.data)
+        this.setState({
+          followers: response.data
+        })
+      })
+  }
 
 
 
 
-  render(){
+
+
+  render() {
     return (
-      <div className = "App">
-        <header className="App-header"></header>
+      <div className="App">
+        <header className="App-header">
+          <UserCard
+            user={this.state.user}//passing props to component
+          />
+
+          {this.state.followers.map(follower => (
+            <div className="followers" key={follower.id}>
+
+              <h1>{follower.login}</h1>
+            </div>
+          ))}
+        </header>
       </div>
     )
   }
--- user-card/src/UserCard.js
+++ user-card/src/UserCard.js
@@ -1,5 +1,14 @@
-// import React from 'react'
+import React from 'react'
+//step2
+const UserCard = props => {
+    console.log(props, "from UserCard")
+    return (
+        <div className="container">
+            <h1>{props.user.name}</h1>
+            <p>{props.user.location}</p>
+            <p>{props.user.followers}</p>
+        </div>
+    )
+}
 
-// const UserCard = props => {
-
-// }
+export default UserCard;
